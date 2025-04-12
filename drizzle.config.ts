fa7222779d7b5295461a84837bab2ec426a5b475
Hugo Protsch <hugo@protsch.dev>
2025-04-12 12:41:51 +0200

    Add singlestore database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
-import { type Config } from "drizzle-kit";
+import {type Config} from "drizzle-kit";
 
-import { env } from "~/env";
+import {env} from "~/env";
 
 export default {
+  out: "./drizzle",
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
+  dialect: "singlestore",
   dbCredentials: {
     url: env.DATABASE_URL,
   },
